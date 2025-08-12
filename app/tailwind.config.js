221f1e59da1573b81dba1155bb2988a6b499f7fc
technicalclipper <technicalclipper@gmail.com>
2025-08-12 14:59:51 +0530

    fix

---
+++ app/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+      },
+      fontFamily: {
+        'press-start-2p': ['var(--font-press-start-2p)', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
